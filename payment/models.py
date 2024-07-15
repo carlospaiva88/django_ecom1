0d3b15b2f8dea713a965b351a3f663ad1ff879d3
Cjr88 <carloscjr70@gmail.com>
2024-07-15 14:28:02 -0300

    shipped/unshipped orders

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 
 class ShippingAdress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -20,6 +23,13 @@ class ShippingAdress(models.Model):
     def __str__(self):
         return f'Shipping Adress - {str(self.id)}'
     
+# Create a User shipping by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAdress(user=instance)
+        user_shipping.save()
+post_save.connect(create_shipping, sender=User)
+
 # Create Order Model
 class Order(models.Model):
     # Foreign Key
@@ -29,9 +39,21 @@ class Order(models.Model):
     shipping_adress = models.TextField(max_length=15000)
     amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f'Order - {str(self.id)}'
+
+# Auto add Shipping date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
+
 
     # Create Ordem Items Model
 class OrderItem(models.Model):
