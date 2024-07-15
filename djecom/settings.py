a2e4b0a5402e58dd8d11b0ce67d8c8e141494067
Cjr88 <carloscjr70@gmail.com>
2024-07-15 16:55:07 -0300

    added DB password environ

--- djecom/settings.py
+++ djecom/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '19693',
         
