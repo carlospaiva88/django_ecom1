2d629ecd59895510d52f0f7bc0571e799395ebd1
carlospaiva88 <carloscjr70@gmail.com>
2024-07-19 12:46:54 -0300

    adding some products to test

--- djecom/settings.py
+++ djecom/settings.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import os 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Load our environmental variables
-# load_dotenv()
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -89,7 +89,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '19693',
         
