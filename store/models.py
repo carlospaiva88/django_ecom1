71532871e982b931bdcc302f8923c5d45e1411fe
carlospaiva88 <carloscjr70@gmail.com>
2024-12-10 10:53:43 -0300

    changing layout cards

--- store/models.py
+++ store/models.py
@@ -37,7 +37,8 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = 'categories'
-    
+
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
